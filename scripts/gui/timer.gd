edbcc860e3a7f009aff0611355ae63adeaeff7e4
GreenyMushrooms <ethereal@sfr.fr>
2026-06-03 18:31:20 +0200

    fixed minor warnings

--- scripts/gui/timer.gd
+++ scripts/gui/timer.gd
@@ -15,11 +15,11 @@ func _physics_process(delta: float) -> void:
 	milliseconds += int(delta * 1000)
 
 	if milliseconds >= 1000:
-		seconds += milliseconds / 1000.0
+		seconds += milliseconds / 1000
 		milliseconds = milliseconds % 1000
 
 	if seconds >= 60:
-		minutes += seconds / 60.0
+		minutes += seconds / 60
 		seconds = seconds % 60
 	label.text = "%02d:%02d:%03d" % [minutes, seconds, milliseconds]
 	
