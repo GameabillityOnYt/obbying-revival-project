8e986b45523a17231669b6933454619ff6c47ece
Oliver_Melon <oliverjnicoloff@gmail.com>
2026-07-01 13:28:06 -0500

    Remove slider enabling from menu return functions
    Removed calls to set_sliders_enabled for menu transitions.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -425,7 +425,6 @@ func _on_settings_pressed() -> void: # when you press settings it makes your cam
 func _on_return_to_main_pressed() -> void:
 	$Camera2D.position_smoothing_enabled = GameManager.data.menuTransitions
 	current_page = "main"
-	GameManager.set_sliders_enabled(false)
 	cam.global_position = Main.global_position
 	
 	if search != null:
@@ -437,19 +436,16 @@ func _on_return_to_main_pressed() -> void:
 func _on_return_to_settings_pressed() -> void:
 	$Camera2D.position_smoothing_enabled = GameManager.data.menuTransitions
 	current_page = "settings"
-	GameManager.set_sliders_enabled(true)
 	cam.global_position = Settings.global_position
 
 func _on_avatar_pressed() -> void:
 	$Camera2D.position_smoothing_enabled = GameManager.data.menuTransitions
 	current_page = "avatar"
-	GameManager.set_sliders_enabled(false)
 	cam.global_position = AvatarCustom.global_position
 
 func _on_help_pressed() -> void:
 	$Camera2D.position_smoothing_enabled = GameManager.data.menuTransitions
 	current_page = "help"
-	GameManager.set_sliders_enabled(false)
 	cam.global_position = Help.global_position
 
 
